e19bc920d998943920661b119e91f39688df32b4
Водолагин Артём <52680620+Xainqq@users.noreply.github.com>
2022-03-28 02:45:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,60 +1,68 @@
-// Copyright 2021 Xainqq
+// Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len-1; i++) {
-    for (int j = i+1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count++;
-      }
-    }
-  }
-  return count;
+int count = 0;
+
+for (int i = 0; i < len-1; i++) {
+for (int j = i + 1; j < len; j++) {
+if (arr[i] + arr[j] == value) {
+count++;
+}
+}
+}
+return count;
+return 0;
 }
 
 int countPairs2(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len-1; i++) {
-    for (int j = len-1; j > i; j--) {
-      if (arr[i] + arr[j] == value) {
-        count++;
-      }
-    }
-  }
-  return count;
-}
+int count = 0;
 
-int cbinsearch(int *arr, int size, int crit, int value) {
-  int count = 0;
-  int l = crit;
-  int r = size - 1;
-  int mid = 0;
-  while (l <= r) {
-    mid = l + (r - l) / 2;
-    if (arr[mid] == value && mid != crit) {
-      count++;
-      int m1 = mid, m2 = mid;
-      while (arr[--m1] == value && m1 > crit) {
-          count++;
-      }
-      while (arr[++m2] == value) {
-        count++;
-      }
-      return count;
-    } else if (arr[mid] > value) {
-      r = mid - 1;
-    } else {
-      l = mid + 1;
-    }
-  }
-  return 0;
+for (int i = 0; i < len-1; i++) {
+if(arr[i] > value) { break; }
+for (int j = len-1; j > i; j--) {
+if (arr[j] > value) { continue; }
+if (arr[i] + arr[j] == value) {
+count++;
+}
+}
+}
+return count;
+return 0;
 }
 
 int countPairs3(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    if (arr[i] <= value - arr[i]) {
-      count += cbinsearch(arr, len, i, value - arr[i]);
-    }
-  }
-  return count;
+int count = 0;
+int right = len - 1;
+int left = 0;
+int index = 0;
+int mid;
+
+for (int i = 0; i < len - 1; i++) {
+int prim = 0;
+left = i + 1;
+mid = (left + right) / 2;
+while (left <= right) {
+if (arr[mid] + arr[i] == value) {
+count++;
+prim = 1;
+break;
+}
+if (arr[mid] + arr[i] > value) {
+right = mid - 1;
+} else { left = mid + 1; }
+mid = (left + right) / 2;
+}
+if (prim) {
+int middle = mid;
+index = mid;
+while (arr[index + 1] + arr[i] == value) {
+count++;
+index++;
+}
+while (arr[middle - 1] + arr[i] == value && middle - 1 != i) {
+count++;
+middle--;
+}
+}
+}
+return count;
 }
